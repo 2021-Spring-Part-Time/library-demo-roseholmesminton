7742e90bade1bbb41f9463394aa36763182eba5c
roseholmesminton <rose.yvonne216@icloud.com>
2021-06-12 13:45:59 -0400

    Finished first wired test

---
+++ src/main/java/org/wecancodeit/librarydemo/Campus.java
@@ -0,0 +1,54 @@
+package org.wecancodeit.librarydemo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Campus {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String location;
+
+
+    @OneToMany(mappedBy = "campus")
+    private Collection<Book> books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+// default constructor required by jpa
+    public Campus(){
+
+    }
+
+    public Campus(String location) {
+        this.location = location;
+    }
+
+
+    public Collection<Book> getBooks() {
+        return books;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Campus campus = (Campus) o;
+        return id.equals(campus.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/org/wecancodeit/librarydemo/JPAWiringTest.java
@@ -0,0 +1,48 @@
+package org.wecancodeit.librarydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class JPAWiringTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    private CampusRepository campusRepo;
+
+    @Autowired
+    private AuthorRepository authorRepo;
+
+    @Autowired
+    private BookRepository bookRepo;
+
+@Test
+    public void campusShouldHaveAListOfBooks() {
+        Campus testCampus = new Campus("Test Location");
+        Author testAuthor1 = new Author("Test FirstName", "Test LastName");
+        Book testBook = new Book("Title", "Description", testCampus, testAuthor1);
+
+        campusRepo.save(testCampus);
+        authorRepo.save(testAuthor1);
+        bookRepo.save(testBook);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Optional<Campus> retrievedCampusOpt = campusRepo.findById(testCampus.getId());
+        Campus retrievedCampus = retrievedCampusOpt.get();
+        Book retrievedBook = bookRepo.findById(testBook.getId()).get();
+        assertThat(retrievedCampus.getBooks()).contains(testBook);
+
+    }
+}
